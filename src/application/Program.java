cc8dbff9d669c31b17014c4c3c0a6aac80131290
ionan costa <ionan.costa@innyx.com>
2023-08-28 21:09:01 -0300

    add class board e pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,11 @@
 package application;
 
+import boardgame.Board;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		
+		Board board = new Board(8, 8);
 	}
-
 }
